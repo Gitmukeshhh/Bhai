a016730e2f4604ccd3230c24267f991dafb177c5
Dell <mukeshdhale27@gmail.com>
2023-01-26 12:13:00 -0800

    msg

---
+++ src/song.java
@@ -0,0 +1,9 @@
+public class song {
+    String titl;
+    double duration;
+
+    public song(String name, double duration) {
+        this.titl = name;
+        this.duration = duration;
+    }
+}
